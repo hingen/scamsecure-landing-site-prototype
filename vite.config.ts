f4ecfb705a4502561c0ad91f956036d4a55c5339
hingen <1627313-hingen@users.noreply.gitlab.com>
2023-10-21 18:32:40 +0800

    Fix deployment config

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
   base: "/scamsecure-landing-site-prototype/",
+  plugins: [react()],
 });
